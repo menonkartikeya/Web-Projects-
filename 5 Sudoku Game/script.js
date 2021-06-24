3b31244b8da5b98d15447fff552dd012713370ca
Kartikeya <55075539+menonkartikeya@users.noreply.github.com>
2021-06-24 08:39:42 +0530

    Update script.js

--- 5 Sudoku Game/script.js
+++ 5 Sudoku Game/script.js
@@ -1,17 +1,19 @@
 //Load Borads from file or Manually
 
-const easy = [
+var easy = [
     "6------7------5-2------1---362----81--96-----71--9-4-5-2---651---78----345-------",
     "685329174971485326234761859362574981549618732718293465823946517197852643456137298"
-  ];
-  const medium = [
+];
+var medium = [
     "--9-------4----6-758-31----15--4-36-------4-8----9-------75----3-------1--2--3--",
     "619472583243985617587316924158247369926531478734698152891754236365829741472163895"
-  ];
-  const hard = [
+];
+var hard = [
     "-1-5-------97-42----5----7-5---3---7-6--2-41---8--5---1-4------2-3-----9-7----8--",
     "712583694639714258845269173521436987367928415498175326184697532253841769976352841"
-  ];
+];
+
+
 
 //Game Variables
 var timer;
@@ -28,42 +30,69 @@ window.onload =()=>{
     //set theme 
     id("theme-1").addEventListener('click', changeThemeLight);
     id("theme-2").addEventListener('click', changeThemeDark);
-    changeThemeDark();
+    changeThemeDark();  //makes dark as default theme
     
     for(let i=0; i<id("number-container").children.length; i++){
         // console.log(id("number-container").children[i])
-        id("number-container").children[i].addEventListener("click", function(){
-            // console.log(this)
-            //If selecting is not disabled 
-            if(!disableSelect){
-                
-                //If number is already selected
-                if(this.classList.contains("selected")){
-                    //then remove selection
-                    this.classList.remove("selected");
-                    selectedNum=null;
-                }
-                else{
-                    //Deselect all other numbers
-                    for(let i=0;i<9;i++){
-                        id("number-container").children[i].classList.remove("selected");
-                    }
+        id("number-container").children[i].addEventListener("click", numberContainer)
+    }
 
-                    //Select it and update selectNum variable
-                    this.classList.add("selected");
-                    selectedNum = this;
-                    updateMove();
-                }
+    //Add event listeners to keyboard
+    document.addEventListener('keydown', (event) => {
+        const keyName = event.key;
+      
+        switch(keyName){
+            case '1' : id("number-container").children[0].click();
+                        break;
+            case '2' : id("number-container").children[1].click();
+                        break;
+            case '3' : id("number-container").children[2].click();
+                        break;
+            case '4' : id("number-container").children[3].click();
+                        break;
+            case '5' : id("number-container").children[4].click();
+                        break;
+            case '6' : id("number-container").children[5].click();
+                        break;
+            case '7' : id("number-container").children[6].click();
+                        break;
+            case '8' : id("number-container").children[7].click();
+                        break;
+            case '9' : id("number-container").children[8].click();
+                        break;
+        }
+      }, false);
+      
+}
 
+function numberContainer(){
+    // console.log(this)
+    //If selecting is not disabled 
+    if(!disableSelect){
+        
+        //If number is already selected
+        if(this.classList.contains("selected")){
+            //then remove selection
+            this.classList.remove("selected");
+            selectedNum=null;
+        }
+        else{
+            //Deselect all other numbers
+            for(let i=0;i<9;i++){
+                id("number-container").children[i].classList.remove("selected");
             }
-        })
+
+            //Select it and update selectNum variable
+            this.classList.add("selected");
+            selectedNum = this;
+            updateMove();
+        }
+
     }
 }
 
-
-
 function startGame(){
-    // alert('Start!!!');
+    // alert('Start!!!');        
 
     //Choose Board difficulty
     let board;
@@ -78,6 +107,7 @@ function startGame(){
     lives = 3;
     disableSelect = false;
     id('lives').textContent = "Lives Remaining: 3";
+    id("lives").style.color="inherit";
 
     //Create Board based on difficulty 
     generateBoard(board);
@@ -266,12 +296,15 @@ function endGame(){
 
     //display won or lost message
     if(lives === 0 || timeRemaining === 0){
+        id("lives").style.color="red";
         id("lives").textContent = "You Lost!!";
     }
     else{
-        id("lives").textContent = "You Won!";
+        id("lives").style.color="blue";
+        id("lives").textContent = "Congratulations! You Won!";
     }
     
+    //Make cursors as not-allowed for tiles and number-container 
     for(let i=0; i<qsa(".tile").length; i++)
         qsa(".tile")[i].style.cursor="not-allowed";
     for(let i=0; i<id("number-container").children.length;i++)
